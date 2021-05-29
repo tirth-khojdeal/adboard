b6eef3310af3ec498c395af3cfb24ede25c3191b
Tirth Raj Kumar <stirthraj.12345@gmail.com>
2021-05-29 11:26:44 +0530

    merge expand cols with automate

--- src/components/content/index.js
+++ src/components/content/index.js
@@ -1,32 +1,3 @@
-<<<<<<< HEAD
-import { Paper, Typography } from "@material-ui/core";
-import { Route } from "react-router";
-import Demo from "../Section";
-import './style.css';
-
-export default function Content() {
-
-  return (
-      <Paper className="content-root content-welcome-msg" elevation={3}>
-        <Route exact path="/">
-          <Typography component="div" style={{ height: "50vh" }}>
-            <h1>Welcome to our Board</h1>
-          </Typography>
-        </Route>
-        <Route exact path="/sub1">
-          <h1>sub 1</h1>
-        </Route>
-        <Route exact path="/sub2">
-          <h1>sub 2</h1>
-        </Route>
-        <Route exact path="/sub3">
-          <h1>sub 3</h1>
-        </Route>
-        <Route exact path="/analyze">
-          <Demo />
-        </Route>
-      </Paper>
-=======
 import { Button, Container, Paper, Typography } from "@material-ui/core";
 import { Route } from "react-router";
 import Demo from "../Section";
@@ -70,7 +41,6 @@ export default function Content({togglevalue}) {
         </Paper>
       </Container>
     </div>
->>>>>>> fab964caf614d97ec2f1e097adaa9b5bef47aef5
   );
 }
 
