8183fd9ca34a1612e69f4adf10def273c94aab2c
Tirth Raj Kumar <stirthraj.12345@gmail.com>
2021-05-29 11:25:29 +0530

    merge expand cols with automate

--- src/components/content/index.js
+++ src/components/content/index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { Paper, Typography } from "@material-ui/core";
 import { Route } from "react-router";
 import Demo from "../Section";
@@ -25,6 +26,51 @@ export default function Content() {
           <Demo />
         </Route>
       </Paper>
+=======
+import { Button, Container, Paper, Typography } from "@material-ui/core";
+import { Route } from "react-router";
+import Demo from "../Section";
+import './style.css';
+import Automate from '../automate/automate';
+export default function Content({togglevalue}) {
+
+  return (
+    <div className="content-root">
+      <Container maxWidth="lg">
+        <Paper >
+          <Route exact path="/">
+            <Typography component="div" style={{ height: "50vh" }}>
+              <h1>Welcome to our Board</h1>
+            </Typography>
+          </Route>
+          <Route exact path="/sub1">
+            <h1>sub 1</h1>
+          </Route>
+          <Route exact path="/sub2">
+            <h1>sub 2</h1>
+          </Route>
+          <Route exact path="/sub3">
+            <h1>sub 3</h1>
+          </Route>
+          <Route exact path="/analyze">
+            <Demo />
+          </Route>
+          <Route exact path="/automate">
+            <Automate />
+          </Route>
+          <Route exact path="/reports">
+            <h1>reports</h1>
+          </Route>
+          <Route exact path="/form">
+            <h1>form</h1>
+          </Route>
+          <Route exact path="/table">
+            <h1>table</h1>
+          </Route>
+        </Paper>
+      </Container>
+    </div>
+>>>>>>> fab964caf614d97ec2f1e097adaa9b5bef47aef5
   );
 }
 
